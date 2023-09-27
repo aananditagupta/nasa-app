3a9daeed3fc89cdb2ec21f02e100cd989596a958
Aanandita Gupta <aananditagupta14@gmail.com>
2023-09-27 12:40:16 +0100

    feat: added a temporary favourites page

---
+++ src/components/Favourites.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Favourites = () => {
+  return (
+    <div>
+      {/* Placeholder title */}
+      <Typography variant="h4">Favourites</Typography>
+      
+      {/* Placeholder message */}
+      <Typography>Feature coming soon!</Typography>
+    </div>
+  );
+};
+
+export default Favourites;
